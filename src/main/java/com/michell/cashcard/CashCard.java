00ae98905eb3019ec5ab28d49947a0a37d3f54d6
Michell179 <michellarias173@gmail.com>
2024-05-19 19:06:01 -0500

    Add JPA dependency

--- src/main/java/com/michell/cashcard/CashCard.java
+++ src/main/java/com/michell/cashcard/CashCard.java
@@ -2,7 +2,5 @@ package com.michell.cashcard;
 
 import org.springframework.data.annotation.Id;
 
-import java.math.BigDecimal;
-
 record CashCard(@Id Long id, Double amount, String owner) {
 }
--- src/main/java/com/michell/cashcard/SecurityConfig.java
+++ src/main/java/com/michell/cashcard/SecurityConfig.java
@@ -39,16 +39,6 @@ class SecurityConfig {
                 .password(passwordEncoder.encode("abc123"))
                 .roles("CARD-OWNER")
                 .build();
-        UserDetails hankOwnsNoCards = users
-                .username("hank-owns-no-cards")
-                .password(passwordEncoder.encode("qrs456"))
-                .roles("NON-OWNER")
-                .build();
-        UserDetails kumar = users
-                .username("kumar2")
-                .password(passwordEncoder.encode("xyz789"))
-                .roles("CARD-OWNER")
-                .build();
-        return new InMemoryUserDetailsManager(sarah, hankOwnsNoCards, kumar);
+        return new InMemoryUserDetailsManager(sarah);
     }
 }
